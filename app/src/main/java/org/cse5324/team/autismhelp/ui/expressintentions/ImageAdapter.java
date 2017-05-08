b7236f12751638d84bcb5b95110f70d9c2e27ee2
shaurye07 <bhatnagar.shaurye@gmail.com>
2017-05-08 15:00:04 -0500

    Updated Express intention

--- app/src/main/java/org/cse5324/team/autismhelp/ui/expressintentions/ImageAdapter.java
+++ app/src/main/java/org/cse5324/team/autismhelp/ui/expressintentions/ImageAdapter.java
@@ -1,13 +1,17 @@
 package org.cse5324.team.autismhelp.ui.expressintentions;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import org.cse5324.team.autismhelp.R;
+
+import static android.support.constraint.R.id.parent;
 
 /**
  * Created by salvatore on 6.5.17.
@@ -15,7 +19,7 @@ import org.cse5324.team.autismhelp.R;
 
 public class ImageAdapter extends BaseAdapter {
 
-    private Integer image_id[]={
+    /*private Integer image_id[]={
 
             R.mipmap.aac_me,
             R.mipmap.aac_is,
@@ -63,19 +67,20 @@ public class ImageAdapter extends BaseAdapter {
 
 
 
-    };
+    };*/
+    String griditems[];
     private Context CTX;
 
     public ImageAdapter(Context CTX){
         this.CTX=CTX;
-
+        griditems = CTX.getResources().getStringArray(R.array.expression_grid);
     }
 
 
 
     @Override
     public int getCount() {
-        return image_id.length;
+        return griditems.length;
     }
 
     @Override
@@ -89,10 +94,109 @@ public class ImageAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        ImageView img;
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
 
-        if(view==null){
+        View inflate = LayoutInflater.from(CTX).inflate(R.layout.grid_express_layout,null);
+        ImageAdapter.ViewHolder view =  (ImageAdapter.ViewHolder)inflate.getTag();
+        if(view == null){
+            view = new ImageAdapter.ViewHolder();
+            TextView label = (TextView) inflate.findViewById(R.id.txtv_grid_xpress);
+            ImageView icon = (ImageView) inflate.findViewById(R.id.imgv_grid_xpress);
+            view.label = label;
+            view.icon = icon;
+            inflate.setTag(view);
+        }
+
+        view.label.setText(griditems[i]);
+        if(i==0){
+            view.icon.setImageResource(R.mipmap.aac_me);
+        }else if(i==1){
+            view.icon.setImageResource(R.mipmap.aac_is);
+        }else if(i==2){
+            view.icon.setImageResource(R.mipmap.aac_what);
+        }else if(i==3){
+            view.icon.setImageResource(R.mipmap.aac_where);
+        }else if(i==4){
+            view.icon.setImageResource(R.mipmap.aac_you);
+        }else if(i==5){
+            view.icon.setImageResource(R.mipmap.aac_want);
+        }else if(i==6){
+            view.icon.setImageResource(R.mipmap.aac_like);
+        }else if(i==7){
+            view.icon.setImageResource(R.mipmap.aac_this);
+        }else if(i==8){
+            view.icon.setImageResource(R.mipmap.aac_good);
+        }else if(i==9){
+            view.icon.setImageResource(R.mipmap.aac_bad);
+        }else if(i==10){
+            view.icon.setImageResource(R.mipmap.aac_up);
+        }else if(i==11){
+            view.icon.setImageResource(R.mipmap.aac_down);
+        }else if(i==12){
+            view.icon.setImageResource(R.mipmap.aac_not);
+        }else if(i==13){
+            view.icon.setImageResource(R.mipmap.aac_stop);
+        }else if(i==14){
+            view.icon.setImageResource(R.mipmap.aac_help);
+        }else if(i==15){
+            view.icon.setImageResource(R.mipmap.aac_red);
+        }else if(i==16){
+            view.icon.setImageResource(R.mipmap.aac_green);
+        }else if(i==17){
+            view.icon.setImageResource(R.mipmap.aac_yelllow);
+        }else if(i==18){
+            view.icon.setImageResource(R.mipmap.aac_blue);
+        }else if(i==19){
+            view.icon.setImageResource(R.mipmap.aac_purple);
+        }else if(i==20){
+            view.icon.setImageResource(R.mipmap.aac_pink);
+        }else if(i==21){
+            view.icon.setImageResource(R.mipmap.aac_orange);
+        } else if(i==22){
+            view.icon.setImageResource(R.mipmap.aac_navyblue);
+        }else if(i==23){
+            view.icon.setImageResource(R.mipmap.aac_apple);
+        }else if(i==24){
+            view.icon.setImageResource(R.mipmap.aac_banana);
+        }else if(i==25){
+            view.icon.setImageResource(R.mipmap.aac_water);
+        }else if(i==26){
+            view.icon.setImageResource(R.mipmap.aac_icecrm);
+        }else if(i==27){
+            view.icon.setImageResource(R.mipmap.aac_beef);
+        }else if(i==28){
+            view.icon.setImageResource(R.mipmap.aac_chicken);
+        }else if(i==29){
+            view.icon.setImageResource(R.mipmap.aac_lobster);
+        }else if(i==30){
+            view.icon.setImageResource(R.mipmap.aac_pie);
+        }else if(i==31){
+            view.icon.setImageResource(R.mipmap.aac_happy);
+        }else if(i==32){
+            view.icon.setImageResource(R.mipmap.aac_sad);
+        }else if(i==33){
+            view.icon.setImageResource(R.mipmap.aac_angry);
+        }else if(i==34){
+            view.icon.setImageResource(R.mipmap.aac_nervous);
+        }else if(i==35){
+            view.icon.setImageResource(R.mipmap.aac_hungry);
+        }else if(i==36){
+            view.icon.setImageResource(R.mipmap.aac_surprised);
+        }else if(i==37){
+            view.icon.setImageResource(R.mipmap.aac_sick);
+        }else if(i==38){
+            view.icon.setImageResource(R.mipmap.aac_sleepy);
+        }
+
+
+        return inflate;
+    }
+
+    private class ViewHolder{
+        TextView label;
+        ImageView icon;
+    }
+        /*if(view==null){
             img=new ImageView(CTX);
             img.setLayoutParams(new GridView.LayoutParams(160,160));
             img.setScaleType(ImageView.ScaleType.CENTER_CROP);
@@ -104,6 +208,6 @@ public class ImageAdapter extends BaseAdapter {
 
         }
         img.setImageResource(image_id[i]);
-        return img;
-    }
+        return img;*/
+
 }
